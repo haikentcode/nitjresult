03dd0d28db49b8d674ea608c2c3da133236cec9b
haikentcode <hiteshnitj16@gmail.com>
2015-04-06 14:50:53 +0530

    new update

--- nitjpage.php
+++ nitjpage.php
@@ -92,17 +92,25 @@ $hk=mysql_connect("localhost","haikent","gudan");
 
 
      $table='<table class="rtable">';
-     
-       $data="";
+     $data='<tr>
+                <td><input value="RollNumber"></td>
+                <td><input value="M1"></td>
+                <td><input value="M2"></td>
+                <td><input value="MJ"></td>
+                <td><input value="TA"></td>
+                <td><input value="Total"></td>
+                <td><input value="Grade"></td>
+            </tr>';
       while($row=mysql_fetch_array($result)){
 
           $data.='<tr id="'.$row['id'].'">';
-          $data.='<td>'.$row['RollNumber'].'</td>';  
-          $data.='<td>'.$row['M1'].'</td>';
-          $data.='<td>'.$row['M2'].'</td>';
-          $data.='<td>'.$row['MJ'].'</td>';
-          $data.='<td>'.$row['Total'].'</td>';
-          $data.='<td>'.$row['Grade'].'</td>';
+          $data.='<td><input value="'.$row['RollNumber'].'"></td>';  
+          $data.='<td><input value="'.$row['M1'].'"></td>';
+          $data.='<td><input value="'.$row['M2'].'"></td>';
+          $data.='<td><input value="'.$row['MJ'].'"></td>';
+          $data.='<td><input value="'.$row['TA'].'"></td>';
+          $data.='<td><input value="'.$row['Total'].'"></td>';
+          $data.='<td><input value="'.$row['Grade'].'"></td>';
           $data.='<td><button>ok</button></td>';
           $data.='</tr>';
           
